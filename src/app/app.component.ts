545c102bd6798188b02155d1c1390a9196ade4bc
Русских Евгения <russkih.evgeniya92@gmail.com>
2020-03-25 11:38:53 +0300

    components 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
 
+export interface Post {
+  title: string;
+  text: string;
+  id?: number;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-basics';
+  posts: Post[] = [
+    {
+      title: 'Пост номер один',
+      text: 'Большой и страшный текст',
+      id: 1
+    },
+    {
+      title: 'Пост под номером два',
+      text: 'Очень важная информация',
+      id: 2
+    },
+    {
+      title: 'Пост нумбер три',
+      text: 'Текст для Алешки'
+    }
+  ];
 }
--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,41 +1,18 @@
-import { Component } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {Post} from '../app.component';
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.scss']
 })
-export class PostComponent {
-  title = 'Post Title';
-  img1 = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTi5dnTYF_TDQRVmu6lurm9SGaTXUSiVB7f968ax22h2FyH-wvllw&s';
-  img2 = 'https://www.seekpng.com/png/full/215-2151945_open-cat-flat-icon-png.png';
-  img = this.img1;
-  firstImg = true;
-  status = '';
-  statusStyle = {};
-  imgActive = false;
+export class PostComponent implements OnInit {
 
-  updateImg() {
-    const time = 400;
-    this.status = 'loading';
-    const addPoint = setInterval(() => {
-      this.status = this.status + '.';
-    }, time);
-    setTimeout(() => {
-      clearInterval(addPoint);
-      if (this.firstImg) {
-        this.img = this.img2;
-      } else {
-        this.img = this.img1;
-      }
-      this.firstImg = !this.firstImg;
-      this.imgActive = !this.imgActive;
-      this.status = 'ready!';
-      this.statusStyle = {color: 'green'};
-      setTimeout(() => {
-        this.status = '';
-        this.statusStyle = {};
-      }, time * 2);
-    }, time * 6);
+  @Input() post: Post;
+
+  constructor() { }
+
+  ngOnInit(): void {
   }
+
 }
