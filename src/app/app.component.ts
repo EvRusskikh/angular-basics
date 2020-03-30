302cf967524056e5d146876bc811425a1590c8f9
Русских Евгения <russkih.evgeniya92@gmail.com>
2020-03-30 12:27:44 +0300

    remove for block3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,24 +12,4 @@ export interface Post {
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  posts: Post[] = [
-    {
-      title: 'Пост номер один',
-      text: 'Большой и страшный текст',
-      id: 1
-    },
-    {
-      title: 'Пост под номером два',
-      text: 'Очень важная информация',
-      id: 2
-    },
-    {
-      title: 'Пост нумбер три',
-      text: 'Текст для Алешки'
-    }
-  ];
-
-  updatePosts(post: Post) {
-    this.posts.unshift(post);
-  }
 }
