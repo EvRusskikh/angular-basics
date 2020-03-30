bf85ed5e70a0a6a737aa6ece0b7e1cfa59598e1b
Русских Евгения <russkih.evgeniya92@gmail.com>
2020-03-30 15:44:46 +0300

    directives 4

--- src/app/directives/style.directive.ts
+++ src/app/directives/style.directive.ts
@@ -1,23 +1,28 @@
-import {Directive, ElementRef, HostListener, Renderer2} from '@angular/core';
+import {Directive, ElementRef, HostListener, Input, Renderer2} from '@angular/core';
 
 @Directive({
   selector: '[appStyle]'
 })
 export class StyleDirective {
 
+  @Input() dStyle: {border?: string, borderRadius?: string, color?: string}
+
   constructor(private el: ElementRef, private r: Renderer2) {
   }
 
   @HostListener('click', ['$event.target']) onClick(event: Event) {
-    console.log(event);
   }
 
   @HostListener('mouseenter') onEnter() {
-    this.r.setStyle(this.el.nativeElement, 'color', 'blue');
+    this.r.setStyle(this.el.nativeElement, 'color', this.dStyle.color);
+    this.r.setStyle(this.el.nativeElement, 'border-radius', this.dStyle.borderRadius);
+    this.r.setStyle(this.el.nativeElement, 'border', this.dStyle.border);
   }
 
   @HostListener('mouseleave') onLeave() {
-    this.r.setStyle(this.el.nativeElement, 'color', '');
+    this.r.setStyle(this.el.nativeElement, 'color', null);
+    this.r.setStyle(this.el.nativeElement, 'border-radius', null);
+    this.r.setStyle(this.el.nativeElement, 'border', null);
   }
 
 }
