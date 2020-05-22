4fd80dadab2d89dc27eabb7aaa44545cd073aaba
Русских Евгения <russkih.evgeniya92@yandex.ru>
2020-05-22 15:40:19 +0300

    reset for services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {FormsModule} from '@angular/forms';
-import { MultPipe } from './pipes/mult.pipe';
-import { UserNamePipe } from './pipes/user-name.pipe';
-import { FilterPipe } from './pipes/filter.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MultPipe,
-    UserNamePipe,
-    FilterPipe,
   ],
   imports: [
     BrowserModule,
