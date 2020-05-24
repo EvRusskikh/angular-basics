841ea4f8ef003b89952ddbbb4622ed4d28df0f99
Русских Евгения <russkih.evgeniya92@yandex.ru>
2020-05-24 16:11:40 +0300

    form 13

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,54 +7,11 @@ import {MyValidators} from './my.validators';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-  form: FormGroup;
-  ngOnInit() {
-    this.form = new FormGroup({
-      email: new FormControl('', [
-        Validators.email,
-        Validators.required,
-        MyValidators.restrictedEmails
-      ], [MyValidators.uniqEmail]),
-      password: new FormControl('', [
-        Validators.required,
-        Validators.minLength(6)
-      ]),
-      address: new FormGroup({
-        country: new FormControl('ru'),
-        city: new FormControl('', [
-          Validators.required
-        ])
-      }),
-      skills: new FormArray([])
-    });
-  }
+export class AppComponent {
+  appState = 'off';
 
-  submit() {
-    if (this.form.valid) {
-      console.log('Form submitted:', this.form);
-      const formData = {...this.form.value};
-      console.log('Form Data:', formData);
-      this.form.reset();
-    }
-  }
-
-  setCapital() {
-    const cityMap = {
-      ru: 'Москва',
-      ua: 'Киев',
-      by: 'Минск'
-    };
-
-    const cityKey = this.form.get('address').get('country').value;
-    const city = cityMap[cityKey];
-
-    this.form.patchValue({address: {city}});
-  }
-
-  addSkill() {
-    const control = new FormControl('', Validators.required);
-    (this.form.get('skills') as FormArray).push(control);
+  handleChange() {
+    console.log(this.appState);
   }
 }
 
