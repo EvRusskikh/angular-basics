a525fdfc0d6d5dc4f8ff86c1220ec854c0a30243
Русских Евгения <russkih.evgeniya92@yandex.ru>
2020-05-25 18:56:44 +0300

    httpClient 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import {delay} from 'rxjs/operators';
 
 export interface Todo {
   completed: boolean;
@@ -15,17 +16,14 @@ export interface Todo {
 export class AppComponent implements OnInit {
 
   todos: Todo[] = [];
+  loading: false;
   todoTitle = '';
 
   constructor(private http: HttpClient) {
   }
 
   ngOnInit() {
-    this.http.get<Todo[]>('https://jsonplaceholder.typicode.com/todos?_limit=2')
-      .subscribe(todos => {
-        console.log('Response:', todos);
-        this.todos = todos;
-      });
+    this.fetchTodos();
   }
 
   addTodo() {
@@ -41,6 +39,17 @@ export class AppComponent implements OnInit {
         console.log('todo:', todo);
         this.todos.push(todo);
         this.todoTitle = '';
+      });
+  }
+
+  fetchTodos() {
+    this.loading = true;
+    this.http.get<Todo[]>('https://jsonplaceholder.typicode.com/todos?_limit=2')
+      .pipe(delay(1500))
+      .subscribe(todos => {
+        console.log('Response:', todos);
+        this.todos = todos;
+        this.loading = false;
       });
   }
 }
