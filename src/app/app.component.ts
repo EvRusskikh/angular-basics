fa00ac0231b9e2a8c8d7893474808c0b6f90b786
Русских Евгения <russkih.evgeniya92@yandex.ru>
2020-05-25 18:46:13 +0300

    httpClient 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,6 +15,7 @@ export interface Todo {
 export class AppComponent implements OnInit {
 
   todos: Todo[] = [];
+  todoTitle = '';
 
   constructor(private http: HttpClient) {
   }
@@ -24,6 +25,22 @@ export class AppComponent implements OnInit {
       .subscribe(todos => {
         console.log('Response:', todos);
         this.todos = todos;
+      });
+  }
+
+  addTodo() {
+    if (!this.todoTitle.trim()) {
+      return;
+    }
+    const newTodo = {
+      title: this.todoTitle,
+      completed: false
+    };
+    this.http.post<Todo>('https://jsonplaceholder.typicode.com/todos', newTodo)
+      .subscribe(todo => {
+        console.log('todo:', todo);
+        this.todos.push(todo);
+        this.todoTitle = '';
       });
   }
 }
