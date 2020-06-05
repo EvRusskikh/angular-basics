35cd8183daf359caf049f503650a12115cc3af43
Русских Евгения <russkih.evgeniya92@yandex.ru>
2020-06-05 16:05:38 +0300

    routing 13

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,13 @@
-import {NgModule} from '@angular/core'
-import {RouterModule, Routes} from '@angular/router'
-import {HomeComponent} from './home/home.component'
-import {AboutComponent} from './about/about.component'
-import {PostsComponent} from './posts/posts.component'
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {AboutComponent} from './about/about.component';
+import {PostsComponent} from './posts/posts.component';
 import {PostComponent} from './post/post.component';
 import {AboutExtraComponent} from './about-extra/about-extra.component';
 import {ErrorPageComponent} from './error-page/error-page.component';
 import {AuthGuard} from './auth.guard';
+import {PostResolver} from './post.resolver';
 
 // http://localhost:4200/ -> HomeComponent
 // http://localhost:4200/about -> AboutComponent
@@ -18,7 +19,13 @@ const routes: Routes = [
     {path: 'extra', component: AboutExtraComponent}
   ]},
   {path: 'posts', component: PostsComponent, canActivate: [AuthGuard]},
-  {path: 'posts/:id', component: PostComponent},
+  {
+    path: 'posts/:id',
+    component: PostComponent,
+    resolve: {
+      post: PostResolver
+    }
+  },
   {path: 'error', component: ErrorPageComponent},
   {path: '**', redirectTo: '/error'},
 ];
